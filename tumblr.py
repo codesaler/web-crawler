05112adcf918e2fe24f908c88a0d8582e6f059fa
Rafael Hoffmann de Barros <rafaelh.debarros@hotmail.com>
2012-04-22 08:35:57 -0700

    Update tumblr.py

--- tumblr.py
+++ tumblr.py
@@ -1,59 +1,71 @@
 #-*- coding:utf-8 -*-
-from xml.dom import minidom
-import urllib
-from PIL import Image
 
-class Tumblr:
-    def __init__(self, site="http://bonjourmadame.fr/api/read", pasta="/Users/rafael/tumblr/"):
-        import glob
+import urllib
+import sys
+from xml.dom import minidom
+import Image
+from glob import glob
+
+class Tumblr(object):
+    def __init__(self, site="http://bonjourmadame.fr/api/read", pasta="/Users/rafael/Dropbox/Bonjour/"):
         self.site = site
         self.pasta = pasta
-        self.counter = len(glob.glob(pasta+"*"))
-        self.paginador = 20
-        self.page = ""
-        self.links = []
+        self.links = {}
+        self.pagina_atual = '?type=photo'
+        import os
+        os.chdir(pasta)
+        self._total_posts()
     
-    def abrir(self):
-        raw_xml = urllib.urlretrieve((self.site+self.page))
-        print raw_xml
-        print raw_xml[0]
-        print raw_xml[1]
+    def _open(self):
+        raw_xml = urllib.urlretrieve(self.site+self.pagina_atual)
         xml = minidom.parse(raw_xml[0])
-        posts = xml.getElementsByTagName("posts")[0]
-        for post in posts.childNodes:
-            link = [x for x in post.childNodes if x.tagName == u'photo-url' 
-                                 and int(x.getAttribute("max-width"))>=500]
-            if link:
-                self.links.append(link[0].childNodes[0].wholeText)
+        self.data = xml.getElementsByTagName('posts')[0]
     
-    def salvar(self):
-        for link in self.links:
-            data = urllib.urlretrieve(link)
-            img = Image.open(data[0])
-            print "Salvando: "+self.pasta+str(self.counter)+"."+img.format.lower()
-            self.counter += 1
-            img.save(self.pasta+str(self.counter)+"."+img.format.lower())
-        self.links = []
+    def _total_posts(self):
+        self._open()
+        self.total_posts = int(self.data.getAttribute("total"))
+        self.original_posts = int(self.data.getAttribute("total"))
+        self._nova_pagina()
     
-    def proximo_set_de_posts(self):
-        self.page = "?start="+str(self.paginador)
-        print self.site+self.page
-        self.paginador += 20
+    def _process_links(self):
+        self._open()
+        for post in self.data.childNodes:
+            for tag in post.childNodes:
+                if tag.tagName == u'photo-url' and \
+                    int(tag.getAttribute('max-width')) >= 500 and \
+                        post.getAttribute('id') not in self.links.keys():
+                    self.links[post.getAttribute('id')] = tag.firstChild.wholeText
+                    break
+        self._nova_pagina()
     
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 2:
-        t = Tumblr(site = sys.argv[1])
-    elif len(sys.argv) == 3:
-        t = Tumblr(site = sys.argv[1], pasta = sys.argv[2])
-    else:
-        t = Tumblr(pasta="/home/rafael/tumblr/")
-    while True:
-        try:
-            t.abrir()
-            t.salvar()
-            t.proximo_set_de_posts()
-        except Exception as e: 
-            print e
-            break
-    print "Download concluido"
+    def _nova_pagina(self):
+        self.pagina_atual = '?start=%d&type=photo' % (self.total_posts-20)
+        if self.total_posts > 0:
+            self.total_posts -= 20
+        else:
+            self.total_posts = 0
+    
+    def process_all_links(self):
+        print 'This might take a while'
+        while len(set(self.links.keys())) < self.original_posts:
+            self._process_links()
+            sys.stdout.flush()
+            print 'Current status: %d of %d' % (len(set(self.links.keys())), self.original_posts)
+    
+    def save_all_images(self):
+        counter = 1
+        for name,link in self.links.items():
+            if len(glob(str(name)+'.*')):
+                print "Image with id:%s already saved." % (str(name))
+            else:
+                data = urllib.urlretrieve(link)
+                img = Image.open(data[0])
+                print "Saving: %d of %d images." % (counter, self.original_posts)
+                img.save(self.pasta+name+"."+img.format.lower())
+            counter += 1
+        print counter
+    
+if __name__ == '__main__':
+    t = Tumblr()
+    t.process_all_links()
+    t.save_all_images()
